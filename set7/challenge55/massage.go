82abc0fa76601dee2936b7dbd940826d331803b6
Matt Wildig <matt@mattwildig.co.uk>
2016-03-26 18:56:48 +0000

    Fix setting bit to value of other state var

--- set7/challenge55/massage.go
+++ set7/challenge55/massage.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"encoding/binary"
+	"encoding/hex"
+	"fmt"
+	"strings"
 
 	"cryptopals/utils"
+	"cryptopals/utils/text"
 )
 
 // helpers funcs from md4.go
@@ -31,6 +36,16 @@ func ithBit(num uint32, i uint) uint32 {
 	return (num ^ uint32(1 << i)) >> i
 }
 
+// Set bit at position pos in word to be equal to that bit
+// in other, and return it
+func setBitEqual(word, other uint32, pos uint) uint32 {
+	target := uint32(1) << pos
+	set := target & other
+	mask := ^target
+
+	return (mask & word) | set
+}
+
 func firstRoundMassage(block []byte) [16]uint32{
 
 	var words [16]uint32
@@ -40,20 +55,20 @@ func firstRoundMassage(block []byte) [16]uint32{
 	var next uint32
 
 	for i := 0; i < 16; i++ {
-		words[i] = binary.LittleEndian.Uint32(block[i * 4: (i + 1) * 4])
+		words[i] = binary.BigEndian.Uint32(block[i * 4: (i + 1) * 4])
 	}
 
 	// a1,7 = b0,7
 	next = leftrotate(a + f(b,c,d) + words[0], 3)    // as md4
-	next ^=  (ithBit(next, 6) ^ ithBit(b, 6)) << 6   // "fix" new val
+	next = setBitEqual(next, b, 6)                   // "fix" new val
 	words[0] = rightrotate(next, 3) - a - f(b, c, d) // massage input so it will give desired result
 	a = next
 
 	// d1,7 = 0, d1,8 = a1,8, d1,11 = a1,11
 	next = leftrotate(d + f(a,b,c) + words[1], 7)
 	next &^= 1 << 6
-	next ^=  (ithBit(next, 7) ^ ithBit(a, 7)) << 7
-	next ^=  (ithBit(next, 10) ^ ithBit(a, 10)) << 10
+	next = setBitEqual(next, a, 7)
+	next = setBitEqual(next, a, 10)
 	words[1] = rightrotate(next, 7) - d - f(a, b, c)
 	d = next
 
@@ -62,7 +77,7 @@ func firstRoundMassage(block []byte) [16]uint32{
 	next |= 1 << 6
 	next |= 1 << 7
 	next &^= 1 << 10
-	next ^=  (ithBit(next, 25) ^ ithBit(d, 25)) << 25
+	next = setBitEqual(next, d, 25)
 	words[2] = rightrotate(next, 11) - c - f(d, a, b)
 	c = next
 
@@ -80,26 +95,26 @@ func firstRoundMassage(block []byte) [16]uint32{
 	next |= 1 << 7
 	next |= 1 << 10
 	next &^= 1 << 25
-	next ^=  (ithBit(next, 13) ^ ithBit(b, 13)) << 13
+	next = setBitEqual(next, b, 13)
 	words[4] = rightrotate(next, 3) - a - f(b, c, d)
 	a = next
 
 	// d2,14 = 0, d2,19 = a2,19, d2,20 = a2,20, d2,21 = a2,21, d2,22 = a2,22, d2,26 = 1
 	next = leftrotate(d + f(a,b,c) + words[5], 7)
-	next |= 1 << 13
-	next ^=  (ithBit(next, 18) ^ ithBit(a, 18)) << 18
-	next ^=  (ithBit(next, 19) ^ ithBit(a, 19)) << 19
-	next ^=  (ithBit(next, 20) ^ ithBit(a, 20)) << 20
-	next ^=  (ithBit(next, 21) ^ ithBit(a, 21)) << 21
+	next &^= 1 << 13
+	next = setBitEqual(next, a, 18)
+	next = setBitEqual(next, a, 19)
+	next = setBitEqual(next, a, 20)
+	next = setBitEqual(next, a, 21)
 	next |= 1 << 25
 	words[5] = rightrotate(next, 7) - d - f(a, b, c)
 	d = next
 
 	// c2,13 = d2,13, c2,14 = 0, c2,15 = d2,15, c2,19 = 0, c2,20 = 0, c2,21 = 1, c2,22 = 0
 	next = leftrotate(c + f(d,a,b) + words[6], 11)
-	next ^=  (ithBit(next, 12) ^ ithBit(d, 12)) << 12
+	next = setBitEqual(next, d, 12)
 	next &^= 1 << 13
-	next ^=  (ithBit(next, 14) ^ ithBit(d, 14)) << 14
+	next = setBitEqual(next, d, 14)
 	next &^= 1 << 18
 	next &^= 1 << 19
 	next |= 1 << 20
@@ -112,7 +127,7 @@ func firstRoundMassage(block []byte) [16]uint32{
 	next |= 1 << 12
 	next |= 1 << 13
 	next &^= 1 << 14
-	next ^=  (ithBit(next, 16) ^ ithBit(c, 16)) << 16
+	next = setBitEqual(next, c, 16)
 	next &^= 1 << 18
 	next &^= 1 << 19
 	next &^= 1 << 20
@@ -130,9 +145,9 @@ func firstRoundMassage(block []byte) [16]uint32{
 	next &^= 1 << 18
 	next &^= 1 << 19
 	next &^= 1 << 20
-	next ^=  (ithBit(next, 23) ^ ithBit(b, 23)) << 23
+	next = setBitEqual(next, b, 22)
 	next |= 1 << 21
-	next ^=  (ithBit(next, 26) ^ ithBit(b, 26)) << 26
+	next = setBitEqual(next, b, 25)
 	words[8] = rightrotate(next, 3) - a - f(b, c, d)
 	a = next
 
@@ -148,7 +163,7 @@ func firstRoundMassage(block []byte) [16]uint32{
 	next |= 1 << 21
 	next &^= 1 << 22
 	next |= 1 << 25
-	next ^=  (ithBit(next, 29) ^ ithBit(a, 29)) << 29
+	next = setBitEqual(next, a, 29)
 	words[9] = rightrotate(next, 7) - d - f(a, b, c)
 	d = next
 
@@ -161,7 +176,7 @@ func firstRoundMassage(block []byte) [16]uint32{
 	next &^= 1 << 22
 	next &^= 1 << 25
 	next |= 1 << 29
-	next ^=  (ithBit(next, 31) ^ ithBit(d, 31)) << 31
+	next = setBitEqual(next, d, 31)
 	words[10] = rightrotate(next, 11) - c - f(d, a, b)
 	c = next
 
@@ -170,7 +185,7 @@ func firstRoundMassage(block []byte) [16]uint32{
 	next &^= 1 << 19
 	next |= 1 << 20
 	next |= 1 << 21
-	next ^=  (ithBit(next, 22) ^ ithBit(c, 22)) << 22
+	next = setBitEqual(next, c, 22)
 	next |= 1 << 25
 	next &^= 1 << 29
 	next &^= 1 << 31
@@ -181,8 +196,8 @@ func firstRoundMassage(block []byte) [16]uint32{
 	next = leftrotate(a + f(b,c,d) + words[12], 3)
 	next &^= 1 << 22
 	next &^= 1 << 25
-	next ^=  (ithBit(next, 26) ^ ithBit(b, 26)) << 26
-	next ^=  (ithBit(next, 28) ^ ithBit(b, 28)) << 28
+	next = setBitEqual(next, b, 26)
+	next = setBitEqual(next, b, 28)
 	next |= 1 << 29
 	next &^= 1 << 31
 	words[12] = rightrotate(next, 3) - a - f(b, c, d)
@@ -201,7 +216,7 @@ func firstRoundMassage(block []byte) [16]uint32{
 
 	// c4,19 = d4,19, c4,23 = 1, c4,26 = 1, c4,27 = 0, c4,29 = 0, c4,30 = 0
 	next = leftrotate(c + f(d,a,b) + words[14], 11)
-	next ^=  (ithBit(next, 18) ^ ithBit(d, 18)) << 18
+	next = setBitEqual(next, d, 18)
 	next |= 1 << 22
 	next |= 1 << 25
 	next &^= 1 << 26
@@ -223,6 +238,44 @@ func firstRoundMassage(block []byte) [16]uint32{
 	return words
 }
 
-func main() {
+func joinWords(words [16]uint32) []byte {
+	ret := make([]byte, 64)
+	p := ret
+	for _, w := range(words) {
+		binary.BigEndian.PutUint32(p, w)
+		p = p[4:]
+	}
+	return ret
+}
 
+func testBlockIsUnchanged(blk []byte) {
+	ary := firstRoundMassage(blk)
+	massaged := joinWords(ary)
+
+	if bytes.Equal(blk, massaged) {
+		text.PrintGreen("Massaged hash is unchanged")
+	} else {
+		text.PrintRed("Massaged text differs:")
+		fmt.Printf("  original:  %s\n", hex.EncodeToString(blk))
+		fmt.Printf("  massaged:  %s\n", hex.EncodeToString(massaged))
+	}
+}
+
+func main() {
+	colliding_hashes_from_paper := []string{
+	    "4d7a9c83 56cb927a b9d5a578 57a7a5ee de748a3c dcc366b3 b683a020 3b2a5d9f c69d71b3 f9e99198 d79f805e a63bb2e8 45dd8e31 97e31fe5 2794bf08 b9e8c3e9",
+	    "4d7a9c83 56cb927a b9d5a578 57a7a5ee de748a3c dcc366b3 b683a020 3b2a5d9f c69d71b3 f9e99198 d79f805e a63bb2e8 45dd8e31 97e31fe5 f713c240 a7b8cf69",
+	}
+
+	for _, s := range(colliding_hashes_from_paper) {
+		s = strings.Replace(s, " ", "", -1)
+		decoded, err := hex.DecodeString(s)
+		if err != nil {
+			text.PrintRed("Invalid hex string:")
+			fmt.Println(err)
+			continue
+		}
+
+		testBlockIsUnchanged(decoded)
+	}
 }
