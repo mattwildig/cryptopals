d2b8bd78296f22e27ff575dc99cb401d39b1fb5c
Matt Wildig <matt@mattwildig.co.uk>
2016-05-22 23:31:21 +0100

    [31] Tidy timing attack output
     - Add final call to server to check correct signature has been found.
     - Allow specifying partial signature on command line to skip initial
       part (useful for debugging).

--- set4/challenge31/client.go
+++ set4/challenge31/client.go
@@ -3,26 +3,38 @@ package main
 import (
 	"encoding/hex"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"os"
 	"time"
 )
 
 var url_string = "http://localhost:8888/test?file=%s&signature=%s"
 
-const NUM_REQUESTS int = 1
-
-
 func main() {
-
 
 	file := "hello"
 	signature_bytes := make([]byte, 20)
+	pos := 0
 
-	for pos := 0; pos < 20; pos ++ {
+	if len(os.Args) == 2 {
+		starting_bytes, e := hex.DecodeString(os.Args[1])
+		if e != nil {
+			fmt.Println("Error decoding starting bytes, must be valid hex string")
+			os.Exit(1)
+		}
+		fmt.Printf("Starting with (assuming correct):\n\t%s\n", os.Args[1])
+		copy(signature_bytes, starting_bytes)
+		pos = len(starting_bytes)
+	}
+
+	for ; pos < 20; pos ++ {
 
 		max_time := time.Duration(0)
 		this_byte := -1
 		for i:= 0; i < 256; i++ {
+
+			fmt.Printf("\rTrying: %02x", i)
 
 			signature_bytes[pos] = byte(i)
 			signature := hex.EncodeToString(signature_bytes)
@@ -39,7 +51,6 @@ func main() {
 
 			resp.Body.Close()
 
-
 			if request_dur > max_time {
 				max_time = request_dur
 				this_byte = i
@@ -47,9 +58,17 @@ func main() {
 
 		}
 		signature_bytes[pos] = byte(this_byte)
-		fmt.Printf("After char %d: %s\n", pos, hex.EncodeToString(signature_bytes))
+		fmt.Printf("\rAfter char %d: %s\n", pos, hex.EncodeToString(signature_bytes))
 	}
 
-	
-	fmt.Println(signature_bytes)
+	signature_hex := hex.EncodeToString(signature_bytes)
+	fmt.Printf("Final signature: %s\n", signature_hex)
+
+	url := fmt.Sprintf(url_string, file, signature_hex)
+	resp, e := http.Get(url)
+	if e != nil {
+		panic("Error! An error has happened!")
+	}
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Printf("Server response with final signature:\n\t%s\n", body)
 }
