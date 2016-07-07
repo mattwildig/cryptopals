70c886a86ac262a1d25f1d42f5a2054da6a5e958
Matt Wildig <matt@mattwildig.co.uk>
2016-07-07 20:09:08 +0100

    [56] Decode upto 32 bytes

--- set7/challenge56/rc4-biases.go
+++ set7/challenge56/rc4-biases.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"crypto/rc4"
 	"fmt"
 
@@ -8,7 +9,7 @@ import (
 	"cryptopals/utils/text"
 )
 
-var secret = []byte("Stick the kettle on and find me a cup")
+var secret = []byte("Stick the kettle on and find me ") // Max 32 bytes
 
 func rc4Oracle(prefix []byte) []byte {
 	cipher, err := rc4.NewCipher(utils.GenKey(16))
@@ -37,33 +38,61 @@ func maxIndex(results [256]int64) int {
 	return maxIndex
 }
 
+func printResult(result []byte) {
+	for _, c := range(result) {
+		if c == 0 {
+			fmt.Print("\033[31mX\033[m")
+		} else {
+			fmt.Printf("\033[32m%s\033[m", string(c))
+		}
+	}
+	fmt.Print("\033[K")
+}
+
 func main() {
 	text.PrintGreen("Starting")
-	prefix := make([]byte, 15)
 
-	var results16, results32 [256]int64
+	prefix := make([]byte, 16)
+	result := make([]byte, len(secret))
 
-	for i := 0; i < 10000000; i++ {
-		if i % 1000 == 0 {
-			fmt.Printf("\r%d\033[K", i)
+	printResult(result)
+	fmt.Println()
+
+	for position := 0; position < 16; position++ {
+		var results16, results32 [256]int64
+
+		prefix = prefix[1:]
+
+		for i := 0; i < 10000000; i++ {
+			if i % 1000 == 0 {
+				fmt.Printf("\r%d\033[K", i)
+			}
+			c := rc4Oracle(prefix)
+			results16[c[15]]++
+			if len(c) >= 32 {
+				results32[c[31]]++
+			}
 		}
-		c := rc4Oracle(prefix)
-		results16[c[15]]++
-		results32[c[31]]++
+		fmt.Print("\r\033[K")
+
+		// Bias at position 16 is for byte 240
+		decodedChar16 := byte(maxIndex(results16)) ^ byte(240)
+		result[position] = decodedChar16
+
+		if len(prefix) + len(secret) >=32 {
+			// Bias at position 16 is for byte 224
+			decodedChar32 := byte(maxIndex(results32)) ^ byte(224)
+			result[position + 16] = decodedChar32
+		}
+		fmt.Print("\033[1F") // move up 1 line
+		printResult(result)
+		fmt.Print("\033[1E") // move back down
+
 	}
-	fmt.Print("\r\033[K")
-
-	// Bias at position 16 is for byte 240
-	decodedChar16 := byte(maxIndex(results16)) ^ byte(240)
-	fmt.Printf("Decoded char at pos 0: %s\n", string(decodedChar16))
-
-	// Bias at position 16 is for byte 224
-	decodedChar32 := byte(maxIndex(results32)) ^ byte(224)
-	fmt.Printf("Decoded char at pos 16: %s\n", string(decodedChar32))
-
-	if decodedChar16 == secret[0] && decodedChar32 == secret[16] {
-		text.PrintGreen("Correctly decoded secret")
+	if  bytes.Equal(result, secret) {
+		text.PrintGreen("Successfully decoded secret")
 	} else {
 		text.PrintRed("Incorrectly decoded secret")
 	}
+	fmt.Print("\033[K")
 }
