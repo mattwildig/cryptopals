f3b1ca52e5101e700bf5a37b825ec2050cf62807
Matt Wildig <matt@mattwildig.co.uk>
2016-08-01 17:34:06 +0100

    [42] Add option to use real ASN.1 processor
    Adds an option to use Go's ASN.1 processor to create a "real" ASN.1
    block. Enable with the -a option: 's' for simple that simply adds the
    predetermined value (default) and 'p' to use the processor. The result
    is identical whichever option is used.
    
    Also increases the RSA key size used to 2048. Using a 1024 bit key
    doesn't leave enough trailing zeros to calculate an appropriate perfect
    cube value (at least with 8 0xff bytes, it might be possible using fewer
    in the padding).

--- set6/challenge42/Bleichenbachers_e3_RSA.go
+++ set6/challenge42/Bleichenbachers_e3_RSA.go
@@ -3,14 +3,37 @@ package main
 import (
 	"bytes"
 	"errors"
+	"flag"
 	"fmt"
 	"math/big"
 
 	"cryptopals/utils"
 )
 
-// Just mock this part for now
-var ASN = []byte("ASN")
+var ASNPrefix = []byte{0x30, 0x21, 0x30, 0x09, 0x06, 0x05, 0x2b, 0x0e, 0x03, 0x02, 0x1a, 0x05, 0x00, 0x04, 0x14}
+var asnSwitch string
+
+const rsaBitLength = 2048
+var rsaByteLength int
+
+
+func init() {
+	flag.StringVar(&asnSwitch, "a", "s", "how to create asn: s: simple, p: processor")
+	flag.Parse()
+
+	rsaByteLength = rsaBitLength / 8
+}
+
+func encodeASN(hash []byte) []byte {
+	switch asnSwitch {
+	case "s":
+		return append(ASNPrefix, hash...)
+	case "p":
+		return createASN(hash)
+	default:
+		panic("Invalid ASN switch")
+	}
+}
 
 // Returns the digest from the formatted data block
 // (this is the broken version, doesn't ckeck if digest is right justified)
@@ -36,32 +59,38 @@ func parse_data(data []byte) ([]byte, error) {
 
 	pos ++
 
-	if ! bytes.Equal(data[pos:pos + 3], ASN) {
-		return nil, errors.New("ASN bytes must be \"ASN\"")
+	if ! bytes.Equal(data[pos:pos + len(ASNPrefix)], ASNPrefix) {
+		return nil, errors.New("Incorrect ASN.1 bytes")
 	}
 
-	pos += 3
+	pos += len(ASNPrefix)
 
 	// Just SHA1 for now (20 bytes)
 	return data[pos:pos +20], nil
 }
 
-// Returns 128 byte (1024 bit) formatted signature block
+// Returns rsaByteLength byte (rsaBitLength bit) formatted signature block
 func create_block(digest []byte) ([]byte, error) {
 	if len(digest) != 20 {
 		return nil, errors.New("Digest should be 20 bytes (SHA1 only)")
 	}
 
-	block := make([]byte, 128)
-	block[0] = 0x00
-	block[1] = 0x01
-	// 102 0xff bytes (128 - 20(hash) - 2(prefix bytes) - 1(zero suffix) - 3(ASN))
-	for i := 2; i < 104; i++ {
-		block[i] = 0xff
+	digest = encodeASN(digest)
+
+	block := make([]byte, rsaByteLength)
+
+	// PKCS1v1.5 padding...
+	p := 0
+	block[p] = 0x00; p++
+	block[p] = 0x01; p++
+	// 0xff bytes (rsaByteLength - 2(prefix bytes) - 1(zero suffix) - len(ASN + hash))
+	// = rsaByteLength - 3 - len(digest)
+	for i := 0; i < rsaByteLength - 3 - len(digest); i++ {
+		block[p] = 0xff; p++
 	}
-	block[104] = 0x00
-	copy(block[105:108], ASN)
-	copy(block[108:], digest)
+	block[p] = 0x00; p++
+
+	copy(block[p:], digest)
 
 	return block, nil
 }
@@ -71,13 +100,14 @@ func verify(rsa utils.RSA, signature, message []byte) bool {
 	block := rsa.EncryptBytes(signature)
 
 	//lpad block to 128 bytes
-	zeros := 128 - len(block)
+	zeros := rsaByteLength - len(block)
 
 	if zeros < 0 {
-		panic("Oops, padding shouldn't be negative")
+		panic("Padding shouldn't be negative")
 	}
 
 	block = append(make([]byte, zeros), block...)
+	fmt.Printf("After encryption (when verifying):\n%x\n", block)
 
 	digest, err := parse_data(block)
 
@@ -91,25 +121,29 @@ func verify(rsa utils.RSA, signature, message []byte) bool {
 	if bytes.Equal(digest, our_digest) {
 		return true
 	}
-
+	fmt.Printf("Digests don't match:\nExpected:\t%x\nActual:\t\t%x\n", our_digest, digest)
 	return false
 }
 
 func make_forgery(message []byte) []byte {
 	digest := utils.SHA1(message)
-	block := make([]byte, 128)
+	block := make([]byte, rsaByteLength)
+	p := 0
 
-	block[0] = 0x00
-	block[1] = 0x01
+	block[p] = 0x00; p++
+	block[p] = 0x01; p++
 	// rfc3447 says there should be at least 8 0xff bytes,
 	// so we'll have that many
-	for i:= 2; i < 10; i ++ {
-		block[i] = 0xff
+	for i:= 0; i < 8; i ++ {
+		block[p] = 0xff; p++
 	}
-	block[10] = 0x00
-	copy(block[11:14], ASN)
+	block[p] = 0x00; p++
 
-	copy(block[14:34], digest)
+	digest = encodeASN(digest)
+
+	copy(block[p:p+len(digest)], digest)
+
+	p += len(digest)
 
 	fmt.Printf("Message to forge hashed and formatted:\n%x\n", block)
 
@@ -135,7 +169,10 @@ func main() {
 
 	fmt.Printf("Hashed and formatted:\n%x\n", block)
 
-	key := utils.CreateRSA(1024, 3)
+	fmt.Print("Creating RSA key...")
+	key := utils.CreateRSA(rsaBitLength, 3)
+	fmt.Println("done")
+
 	// Signing is decrypt
 	signature := key.DecryptBytes(block)
 
---
+++ set6/challenge42/asn1.go
@@ -0,0 +1,42 @@
+// An example of ASN.1 encoding using a processor. Normally this isn't done
+// this way, the ASN.1 prefix is calculated in advance and simply added to
+// the digest (with a check to see the data is the right length). See the Go
+// RSA package source for an example.
+
+package main
+
+import (
+	"encoding/asn1"
+)
+
+// This is the identifier for SHA1, taken from the RFC. Other hash functions
+// have their own identifiers.
+var SHA1Oid asn1.ObjectIdentifier = []int{1,3,14,3,2,26}
+
+// '5' is the tag for a ASN.1 Null value. There doesn't seem to be any other
+// way to create a Null other than like this.
+var NullAsn asn1.RawValue = asn1.RawValue{Tag: 5}
+
+type AlgorithmIdentifier struct {
+	Id asn1.ObjectIdentifier
+	Params asn1.RawValue // This is Null for SHA1 (and all other hash functions).
+}
+
+var SHA1AlgorithmIdentifier = AlgorithmIdentifier{SHA1Oid, NullAsn}
+
+type DigestInfo struct {
+	DigestAlgorithm AlgorithmIdentifier
+	Digest []byte
+}
+
+func createASN(digest []byte) []byte {
+
+	digestInfo := DigestInfo{SHA1AlgorithmIdentifier, digest}
+
+	asn, err := asn1.Marshal(digestInfo)
+
+	if err != nil {
+		panic("Error creating ASN")
+	}
+	return asn
+}
