fecf5ee2151c5c1e65e61aa0526f2306cfdeba18
Matt Wildig <matt@mattwildig.co.uk>
2016-08-06 22:25:56 +0100

    [34] Remove use of utils package
    Changes this challenge to use the newly separate packages rather than
    utils.

--- set5/challenge34/mitm.go
+++ set5/challenge34/mitm.go
@@ -1,35 +1,41 @@
+// MITM Key Fixing attack on Diffiie-Hellman with parameter injection.
+
 package main
 
 import (
 	"fmt"
 	"math/big"
-	"cryptopals/utils"
+
+	"cryptopals/aes"
+	"cryptopals/dh"
+	"cryptopals/hash/sha1"
+	"cryptopals/keys"
 )
 
-var client, server utils.DH_t
+var client, server dh.Key
 
 var server_saved_A, server_saved_Key *big.Int
 
 var client_saved_B, client_saved_Key *big.Int
 
 func init_echo(p, g, A *big.Int) *big.Int {
-	server = utils.InitNewDH(p, g)
+	server = dh.InitNew(p, g)
 	server_saved_A = A
-	server_saved_Key = utils.DHSecret(server, A)
+	server_saved_Key = dh.Secret(server, A)
 
 	return server.Public
 }
 
 func echo(message, iv []byte) ([]byte, []byte) {
-	key := utils.SHA1(server_saved_Key.Bytes())[:16]
-	decoded := utils.AesCbcDecrypt(message, key, iv)
-	decoded, _ = utils.CheckAndStripPKCS7(decoded)
+	key := sha1.Sum(server_saved_Key.Bytes())[:16]
+	decoded := aes.CbcDecrypt(message, key, iv)
+	decoded, _ = aes.CheckAndStripPKCS7(decoded)
 	fmt.Printf("S: message received: %q\n", decoded)
 	response := []byte(fmt.Sprintf("Reponse: %s", decoded))
 
-	new_iv := utils.GenKey(16)
+	new_iv := keys.New(16)
 
-	return utils.AesCbcEncrypt(utils.PKCS7(response, 16), key, new_iv), new_iv
+	return aes.CbcEncrypt(aes.PKCS7(response, 16), key, new_iv), new_iv
 }
 
 func init_echo_mitm(p, g, A *big.Int) *big.Int {
@@ -41,32 +47,32 @@ func init_echo_mitm(p, g, A *big.Int) *big.Int {
 func echo_mitm(message, iv []byte) ([]byte, []byte) {
 	fmt.Println("M: Intercepted message, decrypting")
 	// key is 0, so get big.Int bytes for 0 and hash for AES key
-	key := utils.SHA1(big.NewInt(0).Bytes())[:16]
-	decrypted := utils.AesCbcDecrypt(message, key, iv)
-	decrypted, _ = utils.CheckAndStripPKCS7(decrypted)
+	key := sha1.Sum(big.NewInt(0).Bytes())[:16]
+	decrypted := aes.CbcDecrypt(message, key, iv)
+	decrypted, _ = aes.CheckAndStripPKCS7(decrypted)
 	fmt.Printf("M: message is %q\n", decrypted)
 	
 	response, response_iv := echo(message, iv)
-	decrypted = utils.AesCbcDecrypt(response, key, response_iv)
-	decrypted, _ = utils.CheckAndStripPKCS7(decrypted)
+	decrypted = aes.CbcDecrypt(response, key, response_iv)
+	decrypted, _ = aes.CheckAndStripPKCS7(decrypted)
 	fmt.Printf("M: message from server is %q\n", decrypted)
 
 	return response, response_iv
 }
 
 func main() {
-	client = utils.InitNewDH(utils.P, utils.G)
+	client = dh.InitNew(dh.P, dh.G)
 	client_saved_B = init_echo_mitm(client.P, client.G, client.Public)
-	client_saved_Key = utils.DHSecret(client, client_saved_B)
+	client_saved_Key = dh.Secret(client, client_saved_B)
 
-	key := utils.SHA1(client_saved_Key.Bytes())[:16]
-	iv := utils.GenKey(16)
+	key := sha1.Sum(client_saved_Key.Bytes())[:16]
+	iv := keys.New(16)
 
-	message := utils.AesCbcEncrypt(utils.PKCS7([]byte("Hello, this is a banana"), 16), key, iv)
+	message := aes.CbcEncrypt(aes.PKCS7([]byte("Hello, this is a banana"), 16), key, iv)
 
 	response, response_iv := echo_mitm(message, iv)
 
-	decoded := utils.AesCbcDecrypt(response, key, response_iv)
-	decoded, _ = utils.CheckAndStripPKCS7(decoded)
+	decoded := aes.CbcDecrypt(response, key, response_iv)
+	decoded, _ = aes.CheckAndStripPKCS7(decoded)
 	fmt.Printf("C: response received: %q\n", decoded)
 }
